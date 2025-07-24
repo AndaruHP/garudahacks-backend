4392da223ec1882ff3a54375bdeb858096f82e4a
Andaru Hymawan <130301294+AndaruHP@users.noreply.github.com>
2025-07-24 16:38:21 +0700

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(",")
+  : [];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS not allowed"));
+    }
+  },
+};
 
 const app = express();
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
